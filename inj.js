5fb559498209567480bcb5662c4db96d9d40b650
Lukáš Chábek <lukaschabek@gmail.com>
2019-03-11 19:43:52 +0100

    Went back to hard page reload.

--- inj.js
+++ inj.js
@@ -1,16 +1,13 @@
-//console.log("Here!");
-//setTimeout(clickButtons, 3000);
-//setTimeout(location.reload(), 1000 * 60 * 5);
-setInterval(() => {
-	let admin_parent_node = document.getElementById("count_badge_admin_activity").parentElement;
-	admin_parent_node.childNodes[1].click();
-
+setTimeout(() => {
+	let member_parent_node = document.getElementById("count_badge_requests").parentElement;
+	member_parent_node.childNodes[1].click();
 	setTimeout(() => {
-		let member_parent_node = document.getElementById("count_badge_requests").parentElement;
-		member_parent_node.childNodes[1].click();
-		setTimeout(clickButtons, 2000);
+		clickButtons();
 	}, 2000);
-}, Math.random() * 5000 + 8000);
+}, 2000);
+
+
+setTimeout(location.reload, 5 * 1000 * 60);
 
 function clickButtons()
 {
